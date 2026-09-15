94ea14e135fec64b089f19a358589b05e8b889e7
Saanch Goyal <goyalsaanch@gmail.com>
2026-09-16 00:37:39 +0530

    Update Patient.java

--- src/model/Patient.java
+++ src/model/Patient.java
@@ -5,29 +5,29 @@ public class Patient extends Person {
     private int patientId;
     private String disease;
 
-    // Constructor
+    
     public Patient(int patientId, String name, int age, String phone, String disease) {
         super(name, age, phone);
         this.patientId = patientId;
         this.disease = disease;
     }
 
-    // Getter for patient ID
+    
     public int getPatientId() {
         return patientId;
     }
 
-    // Getter for disease
+    
     public String getDisease() {
         return disease;
     }
 
-    // Setter for disease
+    
     public void setDisease(String disease) {
         this.disease = disease;
     }
 
-    // Display patient details
+    
     public void displayPatient() {
         System.out.println("Patient ID: " + patientId);
         System.out.println("Name: " + name);
